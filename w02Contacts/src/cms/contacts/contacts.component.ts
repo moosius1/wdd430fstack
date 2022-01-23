7ac9aa1e6ed16ef17e8bbbe31b524bab6e3cf849
moosius1 <imoe61@gmail.com>
2022-01-22 21:43:22 -0700

    update

---
+++ w02Contacts/src/cms/contacts/contacts.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,Input,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
