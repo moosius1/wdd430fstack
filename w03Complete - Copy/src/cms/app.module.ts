066df1a11a18faf187504782b38441efcf9cf44a
moosius1 <imoe61@gmail.com>
2022-02-04 22:03:32 -0700

    updates

--- w03Complete - Copy/src/cms/app.module.ts
+++ w03Complete - Copy/src/cms/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { HeaderComponent } from './contacts/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
