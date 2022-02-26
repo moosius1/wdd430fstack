0fd5ef077f3a3c66114aa2994c38cf23b75df033
moosius1 <imoe61@gmail.com>
2022-02-26 14:50:50 -0700

    update w06 issues with .slice() function

--- w06/src/cms/contacts/contact.service.ts
+++ w06/src/cms/contacts/contact.service.ts
@@ -1,13 +1,14 @@
-import { Injectable,EventEmitter } from '@angular/core';
-
+import { Injectable, EventEmitter } from '@angular/core';
 import { Contact } from './contact.model';
 import { MOCKCONTACTS } from './MOCKCONTACTS';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
   contactSelectedEvent = new EventEmitter<Contact>();
+  contactChangedEvent = new EventEmitter<Contact>();
 
   contacts: Contact[]=[];
 
@@ -32,5 +33,22 @@ getContact(id:string): Contact {
 }
 
 
+deleteContact(contact: Contact) {
+
+  if (contact === null || contact === undefined) {
+    return;
+  }
+
+  const pos = this.contacts.indexOf(contact);
+
+  
+  if (pos < 0) {
+    return;
+  }
+ 
+  this.contacts.splice(pos, 1);
+  
+  this.contactChangedEvent.emit(this.contacts.slice());
+}
 
 }
