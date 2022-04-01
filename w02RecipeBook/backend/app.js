9b5e915ecc6d95125ed42f4a2ffd6768fd9e0b79
moosius1 <imoe61@gmail.com>
2022-03-31 18:37:15 -0600

    working week 10& 11

---
+++ w02RecipeBook/backend/app.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+
+// const app = express();
+
+
+
+// app.use(('/recipes',req, res, next) =>{
+//     const recipes = [
+
+//     ];
+//     res.json();
+// });
+
+// module.exports = app;
---
+++ w10/src/server/routes/app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'w10\src\index.html'));
+});
+
+module.exports = router;
